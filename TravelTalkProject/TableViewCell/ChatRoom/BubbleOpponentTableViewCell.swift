32fc72321a62efcb471d9ac904807f9c4822ac4d
nhyeonjeong <hjnam5962@gmail.com>
2024-01-15 05:20:04 +0900

    [FEAT] 톡방 리스트 테이블뷰 구현 #1

---
+++ TravelTalkProject/TableViewCell/ChatRoom/BubbleOpponentTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BubbleOpponentTableViewCell.swift
+//  TravelTalkProject
+//
+//  Created by 남현정 on 2024/01/15.
+//
+
+import UIKit
+
+class BubbleOpponentTableViewCell: UITableViewCell {
+//
+//    @IBOutlet weak var opponentImageView: UIImageView!
+//    @IBOutlet weak var opponentNameLabel: UILabel!
+//    @IBOutlet weak var opponentChatLabel: UILabel!
+//    @IBOutlet weak var dateLabel: UILabel!
+//    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+//    func configureCell(data: Chat) {
+//        opponentImageView.image = UIImage(named: data.)
+//    }
+//    
+}
---
+++ TravelTalkProject/TravelTalk/ChatRoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatRoomViewController.swift
+//  TravelTalkProject
+//
+//  Created by 남현정 on 2024/01/15.
+//
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
